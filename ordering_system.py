02117f03ac49f3a36b75d06038d31504d374b10b
worod odetallah <138366430+WorodOdetallah@users.noreply.github.com>
2024-02-04 18:27:20 +0200

    Update ordering_system.py

--- ordering_system.py
+++ ordering_system.py
@@ -5,44 +5,53 @@ menu = {
     4: {"name": 'soup', "price": 4.50},
     5: {"name": 'sandwich', "price": 4.99}
 }
-order_list = [1, 2] 
 
-def calculate_subtotal(order_list):
+def calculate_subtotal(order):
     subtotal = 0
-    for item in order_list:
-        if item in menu:  # Check if the item number exists in the menu
-            subtotal += menu[item]["price"]
+    for item in order:
+        subtotal += item["price"]
     return subtotal
 
 def calculate_tax(subtotal):
-    """ Calculates the tax of an order """
     tax_percentage = 0.15
-    tax = subtotal * tax_percentage
-    return round(tax, 2)
+    tax = round(subtotal * tax_percentage, 2)
+    return tax
 
-def summarize_order(order_list):
-    """ Summarizes the order """
-    order_summary = [(menu[item]["name"], menu[item]["price"]) for item in order_list]
-    subtotal = calculate_subtotal(order_list)
+def summarize_order(order):
+    subtotal = calculate_subtotal(order)
     tax = calculate_tax(subtotal)
-    total_amount = subtotal + tax
-    # Include subtotal in the summary
-    order_summary.append(('Subtotal', subtotal))
-    order_summary.append(('Tax (15%)', tax))
-    order_summary.append(('Total Amount', total_amount))
-    return order_summary
+    total = round(subtotal + tax, 2)
+    names = [item["name"] for item in order]
+    return names, total
 
 def print_order(order):
-  
-    item_count = len([item for item in order if not item[0].startswith('Subtotal') and not item[0].startswith('Tax') and not item[0].startswith('Total')])
-    print(f'You have ordered {item_count} items')
-    for item in order:
-        print(f"{item[0]}: ${item[1]:.2f}")
+    print('You have ordered ' + str(len(order)) + ' items')
+    items = [item["name"] for item in order]
+    print(items)
+    return items
+
+def display_menu():
+    print("------- Menu -------")
+    for selection in menu:
+        print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price'] : >5}")
+    print()
+
+def take_order():
+    display_menu()
+    order = []
+    count = 1
+    for i in range(3):
+        item = input('Select menu item number ' + str(count) + ' (from 1 to 5): ')
+        count += 1
+        order.append(menu[int(item)])
+    return order
 
 def main():
-    order_list = [4, 2, 1]  # Using the corresponding menu item numbers
-    order_summary = summarize_order(order_list)
-    print_order(order_summary)
+    order = take_order()
+    print_order(order)
+    names, total = summarize_order(order)
+    print("Items ordered:", names)
+    print("Total for the order is: $" + str(total))
 
 if __name__ == "__main__":
     main()
